33ac2c98494aeb9b43ce416963382bb0903d2985
your_username <your_email@gmail.com>
2024-03-20 03:30:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Ask Apex
+
+ **Ask Apex** is a chatbot, that works on scrapped data from knowledge based in http://support.apexsystemsinc.com/kb/index.php The Ask apex uses the following architecture to scrape and index data using pinecone as shown below.
+![Data preperation](./docs/data_prep.png)
+For Summary, it uses OpenAI got 4 to summarize the results as shown below  
+![Data preperation](./docs/retrievel.png)
+
+
+# How to use
+## 1. <a name='Prerequisites'></a>Prerequisites
+
+
+ 1. Clone the repo and use the following command
+ 2. Set up OpenAI and PineCone account and get
+	 - OPENAI_API_KEY
+	 - PINECONE_API_KEY
+	 - PINECONE_ENV
+ 3. Store the variable in  `.env` of your project
+ 4. Set your python environment and do
+	```bash
+	pip install .
+	```
+
+
+## 3. Output
+
+Following screenshots shows the output of this RAG model, this model works fine. As the scrapped data is less and has only few documents, the retrieval and summary modules are working as expected. Need to test on more pages
+
+## 4. Roadmap
+
+- [ ] Create metrics to measure hallucination, relevancy, toxicity and retrieval scores
+- [ ] Design a Cloud Build CI/CD
+- [ ] Initialize data on GCP Compute Engine
+- [ ] Improve Retriever 
+- [ ] Integrate the evaluation process directly in GCP
+- [ ] Export the dashboard from LangSmith to monitor RAG process in GCP
+
---
+++ src/chatbot/bot.py
@@ -0,0 +1,207 @@
+# %%
+import sys
+import langchain
+import json
+
+
+# %%
+import os
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+from langchain import OpenAI
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.conversational_retrieval.prompts import (
+    CONDENSE_QUESTION_PROMPT,
+    QA_PROMPT,
+)
+from langchain.chains.llm import LLMChain
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Pinecone
+
+from dotenv import load_dotenv, find_dotenv
+
+
+class CustomScraper:
+    def __init__(self, base_url, storage_dir="./pages"):
+        self.base_url = base_url
+        self.storage_dir = storage_dir
+        self.ensure_storage_dir_exists()
+
+    def ensure_storage_dir_exists(self):
+        """Ensures the storage directory exists."""
+        if not os.path.exists(self.storage_dir):
+            os.mkdir(self.storage_dir)
+
+    def format_url_for_saving(self, url):
+        """Adjust URL to a safe filename format."""
+        clean_url = url.replace("https://", "").replace("http://", "")
+        forbidden_chars = ["/", "\\", ":", "*", "?", '"', "<", ">", "|"]
+        for char in forbidden_chars:
+            clean_url = clean_url.replace(char, "_")
+        return clean_url
+
+    def fetch_page_and_store(self, url, filename=None):
+        """Fetch a page content and store it locally."""
+        try:
+            result = requests.get(url)
+            if filename is None:
+                filename = self.format_url_for_saving(url)
+            filepath = os.path.join(self.storage_dir, f"{filename}.html")
+            with open(filepath, "wb") as file:
+                file.write(result.content)
+            print(f"Downloaded: {url} -> {filepath}")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to download {url}: {e}")
+
+    def scrape_and_download_links(self):
+        """Scrape all links on the base page and download their content."""
+        try:
+            response = requests.get(self.base_url)
+            soup = BeautifulSoup(response.content, "html.parser")
+            links = soup.find_all("a")
+
+            for link in links:
+                href = link.get("href")
+                if href and not href.startswith("#"):
+                    full_url = urljoin(self.base_url, href)
+                    self.fetch_page_and_store(full_url)
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to access {self.base_url}: {e}")
+
+
+# Example usage
+if __name__ == "__main__":
+    base_url1 = "http://support.apexsystemsinc.com/kb/faq.php?cid=1"
+    scraper = CustomScraper(base_url1)
+    scraper.scrape_and_download_links()
+
+    base_url2 = "http://support.apexsystemsinc.com/kb/faq.php?cid=2"
+    scraper = CustomScraper(base_url2)
+    scraper.scrape_and_download_links()
+
+
+# %%
+import os
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+class DataExtractor:
+    def __init__(self, storage_dir):
+        self.storage_dir = storage_dir
+
+    def _extract_link_from_filename(self, filename):
+        # Reconstruct the original link from the saved filename
+        base_url = "http://support.apexsystemsinc.com/"
+        parts = filename.split("_")
+        reconstructed_path = (
+            "/".join(parts[1:]).replace(".html", "").replace("_", "=").replace("-", "&")
+        )
+        return base_url + reconstructed_path
+
+    def _parse_html_file(self, filepath):
+        with open(filepath, "r", encoding="utf-8") as file:
+            title_text = ""
+            info_text = ""
+            soup = BeautifulSoup(file, "html.parser")
+
+            # Extracting the article title
+            title = soup.find("div", class_="article-title")
+            info = soup.find("div", class_="thread-body")
+            if title and info:
+                title_text = title.get_text(strip=True)
+                info_text = " ".join(info.stripped_strings)
+
+            return title_text, info_text
+
+    def extract_data_to_dataframe(self):
+        data = []
+        for filename in os.listdir(self.storage_dir):
+            if not filename.endswith(".html"):
+                continue
+
+            filepath = os.path.join(self.storage_dir, filename)
+            title, info = self._parse_html_file(filepath)
+            if info == "":
+                continue
+            link = self._extract_link_from_filename(filename)
+
+            data.append({"Article Title": title, "Information": info, "Link": link})
+
+        return pd.DataFrame(data)
+
+
+# Usage
+storage_dir = "./pages"  # Update this path to where your HTML files are stored
+extractor = DataExtractor(storage_dir)
+df = extractor.extract_data_to_dataframe()
+print(df)
+
+# %%
+from langchain_community.document_loaders import DataFrameLoader
+
+loader = DataFrameLoader(df, page_content_column="Information")
+
+# %%
+documents = loader.load()
+
+
+load_dotenv(find_dotenv())
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+# %%
+import pinecone
+
+pc = pinecone.Pinecone(
+    api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV")
+)
+index_name = "apex"
+
+# %%
+docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+
+# %%
+# initialize the LLM
+llm = OpenAI(model_name="gpt-4", temperature=0)
+# the non-streaming LLM for questions
+question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
+
+# %%
+# astreaming llm for the docs
+streaming_llm = OpenAI(
+    streaming=True,
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    verbose=True,
+    temperature=0,
+)
+doc_chain = load_qa_chain(streaming_llm, chain_type="stuff", prompt=QA_PROMPT)
+
+# initialize ConversationalRetrievalChain chabot
+qa = ConversationalRetrievalChain(
+    retriever=docsearch.as_retriever(),
+    combine_docs_chain=doc_chain,
+    question_generator=question_generator,
+)
+
+# %%
+# create an array to store the chat history.
+chat_history = []
+question = input("Hi! Ask me a question about Apex FAQ. ")
+
+# create a loop to ask the chatbot questions
+while True:
+    result = qa({"question": question, "chat_history": chat_history})
+    print("\n")
+    chat_history.append((result["question"], result["answer"]))
+    question = input()
